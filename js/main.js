9d5f85bffac3a4105f93a416096ce7e7f7a626b0
Jaep Cuperus <jaepcuperus@hotmail.com>
2020-05-06 13:41:44 +0200

    Moved functions to utils

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 import { Memory } from "./modules/memory.js";
 import { Timer } from "./modules/timer.js";
 import { Settings } from "./modules/settings.js";
+import { toggleClass, setPageColors } from "./modules/utils.js";
 
 const settings = new Settings(document.querySelector("#settings-form"));
 const memory = new Memory(document.querySelector("#memory"), settings.getSetting('board-size'), settings.getSetting('default-character'));
@@ -11,47 +12,18 @@ memory.registerEventHandler(memory.EVENT_WIN, timer.stopTimer.bind(timer));
 
 // Settings update
 settings.registerSettingsHandler(newSettings => {
-    setColors(newSettings.get('primary-color'), newSettings.get('secondary-color'));
+    setPageColors(newSettings.get('primary-color'), newSettings.get('secondary-color'));
 
-    // Reset memory
+    // Reset memory with new settings
     memory.initialize(newSettings.get('board-size'), newSettings.get('default-character'));
 });
 
-// Settings bar toggle
-document.querySelector("#settings-bar #settings-toggle").addEventListener('click', () => {
-    const settingsBar = document.querySelector("#settings-bar");
-    if (settingsBar.classList.contains('open')) {
-        settingsBar.classList.remove('open');
-    } else {
-        settingsBar.classList.add('open');
-    }
-});
+// Register tettings bar toggle event
+document.querySelector("#settings-bar #settings-toggle").addEventListener(
+    'click', () => toggleClass(document.querySelector("#settings-bar"), "open")
+);
 
-// Reset button
-document.querySelector("#settings-bar #restart").addEventListener('click', () => {
-    memory.initialize(settings.getSetting('board-size'), settings.getSetting('default-character'));
-});
-
-function setColors(primaryColor, secondaryColor) {
-    document.querySelector("body").style.color = primaryColor;
-    document.querySelector("body").style.borderColor = primaryColor;
-    document.querySelector("body").style.backgroundColor = secondaryColor;
-
-    for (let element of document.querySelectorAll(".dynamic-color")) {
-        element.style.color = primaryColor;
-        element.style.borderColor = primaryColor;
-    }
-
-    let style = document.querySelector("#custom-color-sheet");
-    let css = "#memory td.active { background-color: " + primaryColor + "; color: " + secondaryColor + " }";
-
-    if (!style) {
-        style = document.createElement("style");
-        style.id = "custom-color-sheet";
-        document.head.appendChild(style);
-    } else {
-        style.innerHTML = '';
-    }
-
-    style.appendChild(document.createTextNode(css));
-}
+// Register reset button event
+document.querySelector("#settings-bar #restart").addEventListener(
+    'click', () => memory.initialize(settings.getSetting('board-size'), settings.getSetting('default-character'))
+);
--- js/modules/utils.js
+++ js/modules/utils.js
@@ -29,3 +29,47 @@ export function shuffleArray(array) {
     }
     return array;
 }
+
+/**
+ * Toggles an css class on a given element
+ * 
+ * @param {HTMLElement} element element to toggle class on
+ * @param {String} cssClass css to class to toggle
+ */
+export function toggleClass(element, cssClass) {
+    if (element.classList.contains(cssClass)) {
+        element.classList.remove(cssClass);
+    } else {
+        element.classList.add(cssClass);
+    }
+}
+
+/**
+ * Sets the page's primary and secondary colors
+ * 
+ * @param {String} primaryColor primary page color
+ * @param {String} secondaryColor secondary page color
+ */
+export function setPageColors(primaryColor, secondaryColor) {
+    document.querySelector("body").style.color = primaryColor;
+    document.querySelector("body").style.borderColor = primaryColor;
+    document.querySelector("body").style.backgroundColor = secondaryColor;
+
+    for (let element of document.querySelectorAll(".dynamic-color")) {
+        element.style.color = primaryColor;
+        element.style.borderColor = primaryColor;
+    }
+
+    let style = document.querySelector("#custom-color-sheet");
+    let css = "#memory td.active { background-color: " + primaryColor + "; color: " + secondaryColor + " }";
+
+    if (!style) {
+        style = document.createElement("style");
+        style.id = "custom-color-sheet";
+        document.head.appendChild(style);
+    } else {
+        style.innerHTML = '';
+    }
+
+    style.appendChild(document.createTextNode(css));
+}
